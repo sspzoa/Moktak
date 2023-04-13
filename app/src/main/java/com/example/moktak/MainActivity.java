26e362c61b5f7cfa25b691cdf801e77cafc2dfb5
Seungpyo <sspyorea@gmail.com>
2023-04-13 13:43:21 +0900

    portrait fixed

--- app/src/main/java/com/example/moktak/MainActivity.java
+++ app/src/main/java/com/example/moktak/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -52,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         initViews();
         initAnimations();
