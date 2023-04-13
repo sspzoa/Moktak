5793390e1caa8bfe141c5b6966f5e1255a0c3ce0
Seungpyo <sspyorea@gmail.com>
2023-04-13 11:57:10 +0900

    code optimization

--- app/src/main/java/com/example/moktak/MainActivity.java
+++ app/src/main/java/com/example/moktak/MainActivity.java
@@ -7,13 +7,11 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.SoundPool;
-import android.media.effect.Effect;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.preference.PreferenceManager;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
@@ -33,30 +31,74 @@ public class MainActivity extends AppCompatActivity {
     public int vis = 0;
     public int invis = 0;
 
+    private SoundPool mSoundPool;
+    private int mSoundId;
+    private Vibrator vibrator;
+    private SharedPreferences sharedPref;
+    private ImageView moktak;
+    private TextView strike_view;
+    private RelativeLayout background;
+    private TextView auto;
+    private ImageView backlight;
+    private Animation animScale;
+    private Animation animFadeIn;
+    private Animation animFadeOut;
+    private Handler mHandler;
+    private Runnable mRunnable;
+    private Runnable nRunnable;
+    private VibrationEffect vibe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SoundPool mSoundPool;
-        int mSoundId;
-        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        initViews();
+        initAnimations();
+        initSoundPool();
+        initHandlers();
 
-        SharedPreferences sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        strike_cnt = sharedPref.getInt("key", 0);
+        strike_view.setText("Strike: " + strike_cnt);
 
-        ImageView moktak =  findViewById(R.id.moktak);
-        TextView strike_view = findViewById(R.id.strike);
-        RelativeLayout background = findViewById(R.id.background);
-        TextView auto = findViewById(R.id.auto);
-        ImageView backlight = findViewById(R.id.backlight);
+        remain_time = -1;
+        remain_touch = 3;
 
-        final Animation animScale = AnimationUtils
-                .loadAnimation(this, R.anim.anim_scale);
-        final Animation animFadeIn = AnimationUtils
-                .loadAnimation(this, R.anim.anim_fadein);
-        final Animation animFadeOut = AnimationUtils
-                .loadAnimation(this, R.anim.anim_fadeout);
+        mHandler.post(mRunnable);
 
+        background.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                onTouchBackground();
+                return false;
+            }
+        });
+
+        auto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAutoClick();
+            }
+        });
+    }
+
+    private void initViews() {
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        moktak = findViewById(R.id.moktak);
+        strike_view = findViewById(R.id.strike);
+        background = findViewById(R.id.background);
+        auto = findViewById(R.id.auto);
+        backlight = findViewById(R.id.backlight);
+    }
+
+    private void initAnimations() {
+        animScale = AnimationUtils.loadAnimation(this, R.anim.anim_scale);
+        animFadeIn = AnimationUtils.loadAnimation(this, R.anim.anim_fadein);
+        animFadeOut = AnimationUtils.loadAnimation(this, R.anim.anim_fadeout);
+    }
+
+    private void initSoundPool() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             mSoundPool = new SoundPool.Builder()
                     .setMaxStreams(1)
@@ -66,116 +108,84 @@ public class MainActivity extends AppCompatActivity {
         }
 
         mSoundId = mSoundPool.load(getApplicationContext(), R.raw.moktak_sound, 1);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        strike_cnt = sharedPref.getInt("key", 0);
-        strike_view.setText("Strike: " + strike_cnt);
+    }
 
+    private void initHandlers() {
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                updateState();
+                mHandler.postDelayed(this, 1000);
+            }
+        };
+        nRunnable = new Runnable() {
+            @Override
+            public void run() {
+                onTouchBackground();
+                mHandler.postDelayed(this, 1000);
+            }
+        };
+    }
 
-        remain_time = -1;
+    private void updateState() {
+        if (remain_time > 0) {
+            remain_time--;
+        } else if (remain_time == 0) {
+            invis++;
+            if (invis == 1) {
+                System.out.println(invis);
+                backlight.startAnimation(animFadeOut);
+                backlight.setVisibility(View.INVISIBLE);
+                vis = 0;
+            }
+        }
+
+        if (remain_touch == 0) {
+            vis++;
+            if (vis == 1) {
+                backlight.startAnimation(animFadeIn);
+                backlight.setVisibility(View.VISIBLE);
+                invis = 0;
+            }
+        }
         remain_touch = 3;
+    }
 
-        Handler mHandler = new Handler();
-        Runnable mRunnable = new Runnable() {
-            @Override
-            public void run() {
+    private void onTouchBackground() {
+        moktak.startAnimation(animScale);
 
-                if(remain_time > 0)
-                {
-                    remain_time--;
-                } else if(remain_time == 0){
-                    invis++;
-                    if (invis == 1) {
-                        System.out.println(invis);
-                        backlight.startAnimation(animFadeOut);
-                        backlight.setVisibility(View.INVISIBLE);
-                        vis = 0;
-                    }
-                }
+        strike_cnt++;
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("key", strike_cnt);
+        editor.apply();
 
-                if(remain_touch == 0)
-                {
-                    vis++;
-                    if (vis == 1) {
-                        backlight.startAnimation(animFadeIn);
-                        backlight.setVisibility(View.VISIBLE);
-                        invis = 0;
-                    }
-                }
-                remain_touch = 3;
+        remain_time = 1;
+        if (remain_touch > 0) {
+            remain_touch--;
+        }
+        vibe = VibrationEffect.createOneShot(40, 255);
+        vibrator.vibrate(vibe);
+        strike_view.setText("Strike: " + strike_cnt);
+        mSoundPool.play(mSoundId, 1, 1, 1, 0, 1);
+    }
 
-                mHandler.postDelayed(this, 1000);
-            }
-        };
-        mHandler.post(mRunnable);
+    private void onAutoClick() {
+        VibrationEffect vibe = VibrationEffect.createOneShot(50, 255);
+        vibrator.vibrate(vibe);
 
-        background.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                moktak.startAnimation(animScale);
+        i++;
+        if (i % 2 != 0) {
+            mHandler.post(nRunnable);
+            System.out.println(i);
 
-                strike_cnt++;
-                editor.putInt("key", strike_cnt);
-                editor.apply();
+            auto.setText("Auto (ON)");
+            auto.setTextColor(Color.parseColor("#B8E6E1"));
+        } else {
+            mHandler.removeCallbacks(nRunnable);
 
-                remain_time = 1;
-                if(remain_touch > 0) {
-                    remain_touch--;
-                }
-                VibrationEffect vibe = VibrationEffect.createOneShot(40, 255);
-                vibrator.vibrate(vibe);
-                strike_view.setText("Strike: " + strike_cnt);
-                mSoundPool.play(mSoundId, 1, 1, 1, 0, 1);
-                return false;
-            }
-        });
-
-        Handler nHandler = new Handler();
-        Runnable nRunnable = new Runnable() {
-            @Override
-            public void run() {
-
-
-                moktak.startAnimation(animScale);
-
-                strike_cnt++;
-                editor.putInt("key", strike_cnt);
-                editor.apply();
-
-                remain_time = 1;
-                if(remain_touch > 0) {
-                    remain_touch--;
-                }
-                VibrationEffect vibe = VibrationEffect.createOneShot(40, 255);
-                vibrator.vibrate(vibe);
-                strike_view.setText("Strike: " + strike_cnt);
-                mSoundPool.play(mSoundId, 1, 1, 1, 0, 1);
-
-                mHandler.postDelayed(this, 1000);
-            }
-        };
-
-        auto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                VibrationEffect vibe = VibrationEffect.createOneShot(50, 255);
-                vibrator.vibrate(vibe);
-
-                i++;
-                if(i % 2 != 0) {
-                    mHandler.post(nRunnable);
-                    System.out.println(i);
-
-                    auto.setText("Auto (ON)");
-                    auto.setTextColor(Color.parseColor("#B8E6E1"));
-                } else {
-                    mHandler.removeCallbacks(nRunnable);
-
-                    auto.setText("Auto (OFF)");
-                    auto.setTextColor(Color.parseColor("#FFFFFF"));
-                }
-            }
-        });
-
-
+            auto.setText("Auto (OFF)");
+            auto.setTextColor(Color.parseColor("#FFFFFF"));
+        }
     }
 }
